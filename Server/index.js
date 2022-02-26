fb18e645dab24f66790dd5252c9e36e09cf204a8
Chris Sankey <csankey6@gmail.com>
2022-02-25 22:17:34 -0500

    Move cameras to separate config

--- Server/index.js
+++ Server/index.js
@@ -4,20 +4,16 @@ console.log("Starting PiCamHome Server");
 
 const inquirer = require('inquirer');
 const mpvAPI = require('node-mpv');
+const fs = require('fs')
 // where you want to initialise the API
 const mpv = new mpvAPI({}, ["--no-cache", "--untimed", "--no-demuxer-thread", "--ontop", "--no-border", "--geometry=50%x50%"]);
 
 var running = true;
 
-
 console.log("Loading Cameras...");
+console.log("Looking for cameras.json file in working directory...");
 
-var cameras = [{
-    number: 1,
-    port: 5000,
-    rotation: 0,
-
-}]
+var cameras = JSON.parse(fs.readFileSync("./cameras.json"))
 
 console.log("Loaded " + cameras.length + " cameras");
 
@@ -45,13 +41,22 @@ async function main() {
         },
       )).action;
 
+      console.log("\n");
       if(action.startsWith("V")) {
-          console.log("\n");
 
           cam = cameras[choices.indexOf(action)];
           try {
             await mpv.start()
             await mpv.load('udp://192.168.0.11:' + cam.port);
+
+
+            await inquirer.prompt({
+              prefix: '',
+              name: 'action',
+              message: 'Hit enter to continue',
+            })
+
+            await mpv.quit();
             // File is playing!
           } catch (error) {
             console.log(error);
