6945a9bdb151e342f0f9f3fb1459dcdac292d5f2
Chris Sankey <csankey6@gmail.com>
2022-03-06 00:48:21 -0500

    Change picam.camera.service to picam-camera.service

--- Camera/Scripts/picam_setup.sh
+++ Camera/Scripts/picam_setup.sh
@@ -49,10 +49,10 @@ cd ..
 chmod u+x Scripts/picam.sh
 chmod u+x Scripts/cleanup.sh
 sudo cp Services/picam.service /etc/systemd/system/
-sudo cp Services/picam.camera.service /etc/systemd/system/
+sudo cp Services/picam-camera.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable picam.service
-sudo systemctl enable picam.camera.service
+sudo systemctl enable picam-camera.service
 
 (crontab -l 2>/dev/null; echo "0 1 * * * /home/pi/Documents/PiCamHomeSecurity/Camera/Scripts/cleanup.sh") | crontab -
 
