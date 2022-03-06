4154148f1d6a95d1e43548f27cb53f082fccef9b
Chris Sankey <csankey6@gmail.com>
2022-03-06 00:48:27 -0500

    Better logging

--- Camera/PiCam/heartbeat.js
+++ Camera/PiCam/heartbeat.js
@@ -14,30 +14,34 @@ server = dgram.createSocket({
 
 server.on("listening", () => {
     var address = server.address();
-    console.log(`CONNECTED: ${address.address}:${address.port}`);
+    console.log(`HEARTBEAT LISTENER CONNECTED: ${address.address}:${address.port}`);
+    console.log("\n");
 });
 
 server.on("message", (message, info) => {
     if (message + "" === "password") {
-        console.log("PASSWORD RECEIVED FROM " + info.address);
+        console.log("[HEARTBEAT] DETECTED FROM " + info.address);
 
         require("./ipsync");
         require("./controller");
-    } else if (message + "" !== "password") {
-        console.log("RECEIVED:\n" + message);
-        console.log("FROM: " + info.address);
+    } else {
+        console.log("[HEARTBEAT] RECEIVED:\n" + message);
+        console.log("[HEARTBEAT] FROM: " + info.address);
     }
+    console.log("\n");
 });
 
 server.on("error", err => {
-    console.log("ERROR:");
+    console.log("[HEARTBEAT] ERROR:");
     console.log(`${err.stack}`);
+    console.log("\n");
     server.close();
 });
 
 server.on("close", err => {
-    console.log("DISCONNECTED:");
+    console.log("[HEARTBEAT] DISCONNECTED:");
     console.log(`${err.stack}`);
+    console.log("\n");
 });
 
 server.bind(PORT, BROADCAST_ADDRESS, function () {
