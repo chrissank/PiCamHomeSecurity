733da2de53a7fbba8bc6237db5c7491a060efafd
Chris Sankey <csankey6@gmail.com>
2022-03-19 22:33:11 -0400

    Fix minor bugs

--- Server/managers/mpv_manager.js
+++ Server/managers/mpv_manager.js
@@ -2,11 +2,10 @@ const mpvAPI = require("node-mpv");
 var ip = require("ip");
 const inquirer = require("inquirer");
 
-// global settings for all videos
-const mpv = new mpvAPI({}, ["--no-cache", "--untimed", "--no-demuxer-thread", "--no-border", "--geometry=50%x50%"]);
-
 module.exports.viewCamera = async function (cam) {
     try {
+        mpv = new mpvAPI({}, ["--no-cache", "--untimed", "--no-demuxer-thread", "--no-border", "--geometry=50%x50%"]);
+
         await mpv.start();
 
         if (cam.rotation) {
@@ -18,10 +17,12 @@ module.exports.viewCamera = async function (cam) {
         await inquirer.prompt({
             prefix: "",
             name: "action",
-            message: "To close the stream, hit enter. Please, do not close the stream with the program",
+            message: "To close the stream, hit enter.",
         });
 
-        await mpv.quit();
+        try {
+            await mpv.quit();
+        } catch (error) {} // who cares if we have an error on closing? it's closed
     } catch (error) {
         console.log(error);
     }
