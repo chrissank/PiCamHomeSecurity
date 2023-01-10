745db1b2113335cb654be8ee67bfb69a9786c109
chrissank <csankey6@gmail.com>
2023-01-10 00:41:57 -0500

    update cleanup

--- Camera/Scripts/cleanup.sh
+++ Camera/Scripts/cleanup.sh
@@ -1,1 +1,12 @@
-find "/home/pi/Documents/PiCamHomeSecurityFootage/" -type f -name "*.h264" -mtime +14 -delete
+#!/bin/bash
+
+search_dir="/home/pi/Documents/PiCamHomeSecurityFootage"
+
+# Find all .h264 files in the directory
+for file in "$search_dir"/*.h264; do
+  # Check if the file is older than 2 weeks
+  if [[ $(stat -c %Y "$file") -lt $(( $(date +%s) - 14*24*60*60 )) ]]; then
+    # Delete the file if it is older than 2 weeks
+    rm "$file"
+  fi
+done
